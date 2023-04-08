4e3167e6595da82dc05839915d2e217769422a26
greg <go2432@wayne.edu>
2023-04-08 16:05:39 -0500

    fix mol2

--- GHOAT/parameters/convert_gro_to_mol2.py
+++ GHOAT/parameters/convert_gro_to_mol2.py
@@ -1,11 +1,11 @@
 import parmed as pmd
-from parmed.gromacs import GromacsTopologyFile, GromacsGroFile
+
 host_dict = {"OAH":"oa","OAMe":"oam"}
 host_oldresname_dict = {"OAH":"CLP","OAMe":"OCB"}
-host_newresname_dict = {"OAH":"OAH","OAMe":"OAM"}
-hgcoords = "b.gro"
-hcoords = "nolig.gro"
-topol = "topol.top"
+host_newresname_dict = {"OAH":"OCT","OAMe":"OAM"}
+coords = "SYSTEM.crd"
+topol = "SYSTEM.top"
+filetoconvert = "b.gro"
 #import nglview as nv
 from pathlib import Path
 cwd = Path().absolute()
@@ -13,49 +13,31 @@ print(cwd)
 import os
 for root, dirs, files in os.walk(".", topdown=False):
    for name in files:
-      if(os.path.basename(root).split("_")[0] in host_dict and os.path.basename(root).split("_")[1]!="alone"):
-            if(os.path.basename(name) == hgcoords):
-                  print(os.path.basename(root))
-                  print(os.path.basename(name))
-                  components = os.path.basename(root).split("_")
-                  filepath = Path(components[1].replace("G", "lda-guest-")+".mol2")
-                  print(cwd/filepath)
-                  print(os.path.join(root, topol))
-                  print(os.path.join(root, hgcoords))
+      #print(name)
+      #print(os.path.join(root, name))
+      #print(os.path.dirname(root))
+      #print(os.path.basename(root))
+      #print(os.path.basename(name))
+      if("bound" in root and name == coords):
+            print(os.path.join(root, name))
+            print(root)
+            print(((root).split("_")[1]).split("/"))
+            components=(((root).split("_")[1]).split("/"))
 
-                  gmx_top = GromacsTopologyFile(os.path.join(root, topol))
-
-                  gmx_gro = GromacsGroFile.parse(os.path.join(root, hgcoords))
-
-                  gmx_top.box = gmx_gro.box # Needed because .prmtop contains box info
-
-                  gmx_top.positions = gmx_gro.positions
-                  #amber = pmd.load_file(os.path.join(root, topol),os.path.join(root, coords))
-                  guest = gmx_gro["(:MOL)"]
-                  guest.save(str(cwd/filepath), overwrite=True)
-      if(os.path.basename(root).split("_")[0] in host_dict and os.path.basename(root).split("_")[1]=="alone"):
-            if(os.path.basename(name) == hcoords):
-                  print(os.path.basename(root))
-                  print(os.path.basename(name))
-                  components = os.path.basename(root).split("_")
-                  filepath = Path("host-"+host_dict[components[0]]+".mol2")
-                  print(cwd/filepath)
-                  print(os.path.join(root, topol))
-                  print(os.path.join(root, hcoords))
-
-                  gmx_top = GromacsTopologyFile(os.path.join(root, topol))
-
-                  gmx_gro = GromacsGroFile.parse(os.path.join(root, hcoords))
-
-                  gmx_top.box = gmx_gro.box # Needed because .prmtop contains box info
-
-                  gmx_top.positions = gmx_gro.positions
-                  #amber = pmd.load_file(os.path.join(root, topol),os.path.join(root, coords))
-                  host = gmx_gro["(:{})".format(host_oldresname_dict[components[0]])]
-                  for res in host.residues:
-                        if res.name == host_oldresname_dict[components[0]]:
-                              res.name = host_newresname_dict[components[0]]
-                  host.save(str(cwd/filepath), overwrite=True)
+            #filepath = Path("host-"+host_dict[components[0]]+components[1].replace("G", "-lda-guest-")+".pdb")
+            #print(cwd/filepath)
+            amber = pmd.load_file(os.path.join(root, topol),os.path.join(root, coords))
+            amber.residues[0], amber.residues[1] = amber.residues[1], amber.residues[0]
+            #amber= amber["(:{})".format(host_oldresname_dict[components[0]])] + amber["(!:{})".format(host_oldresname_dict[components[0]])]           
+            for res in amber.residues:
+                  if res.name == host_oldresname_dict[components[0]]:
+                        res.name = host_newresname_dict[components[0]]
+            guest = amber["(:MOL)"]
+            host = amber["(:{})".format(host_newresname_dict[components[0]])]
+            hostfilepath = Path("host-"+host_dict[components[0]]+".mol2")
+            guestfilepath = Path(components[1].replace("G", "lda-guest-")+".mol2")
+            host.save(str(cwd/hostfilepath), overwrite=True)
+            guest.save(str(cwd/guestfilepath), overwrite=True)
    #for name in dirs:
    #   print(os.path.join(root, name))
 #amber = pmd.load_file('/home/greg/Desktop/GHOAT.py/GHOAT/equil/guest-5/full.prmtop', '/home/greg/Desktop/GHOAT.py/GHOAT/equil/guest-5/full.inpcrd')
