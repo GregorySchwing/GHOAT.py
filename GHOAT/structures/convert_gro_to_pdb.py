5a66d3d833b3d29ada16bf7d28300ac2ef8ae346
greg <go2432@wayne.edu>
2023-04-08 12:27:00 -0500

    create structure host guest pdbs

---
+++ GHOAT/structures/convert_gro_to_pdb.py
@@ -0,0 +1,29 @@
+import parmed as pmd
+
+host_dict = {"OAH":"oa","OAMe":"oam"}
+
+filetoconvert = "b.gro"
+#import nglview as nv
+from pathlib import Path
+cwd = Path().absolute()
+print(cwd)
+import os
+for root, dirs, files in os.walk(".", topdown=False):
+   for name in files:
+      #print(os.path.join(root, name))
+      #print(os.path.dirname(root))
+      #print(os.path.basename(root))
+      #print(os.path.basename(name))
+      if(os.path.basename(root).split("_")[0] in host_dict):
+            if(os.path.basename(name) == filetoconvert):
+                  print(os.path.basename(root))
+                  print(os.path.basename(name))
+                  components = os.path.basename(root).split("_")
+                  filepath = Path("host-"+host_dict[components[0]]+components[1].replace("G", "-guest")+".pdb")
+                  print(cwd/filepath)
+                  amber = pmd.load_file(os.path.join(root, name))
+                  amber.save(str(filepath), overwrite=True)
+   #for name in dirs:
+   #   print(os.path.join(root, name))
+#amber = pmd.load_file('/home/greg/Desktop/GHOAT.py/GHOAT/equil/guest-5/full.prmtop', '/home/greg/Desktop/GHOAT.py/GHOAT/equil/guest-5/full.inpcrd')
+#amber.save('../output/CHARMM/restraints/host_restraints.pdb', overwrite=True)
